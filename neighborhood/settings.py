6c07454473e6098e4618e4bac889e55c3dcbbe4d
MosesGakuhi1857 <kingorimoses386@gmail.com>
2021-11-01 21:13:19 +0300

    Fixed some bug

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -172,7 +172,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
